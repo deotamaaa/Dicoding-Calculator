8ebb887e709f7555102100e0103af26f5e2ab7e3
Deotama <deotama11@gmail.com>
2021-05-18 18:30:22 +0700

    adding some localstorage for the calculator

--- kalkulator.js
+++ kalkulator.js
@@ -88,6 +88,13 @@ function performCalculation() {
     result =
       parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
   }
-
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result,
+  };
+  putHistory(history);
   calculator.displayNumber = result;
+  renderHistory();
 }
